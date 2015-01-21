259b5ed91119c75eb84178675ecfa257b5104309
Corina Putinar <cputinar@gmail.com>
2015-01-20 17:53:21 -0800

    Working on Program 1 and Program 3

--- Program3.c
+++ Program3.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 
 
-//Fork-variable-test: 	
-//File-descriptor-test: file descriptors in parent and child have the 
+//Fork-variable-test: 
+//File-descriptor-test:  
 void Fork-variable-test();
 void File-descriptor-test();
 
@@ -13,9 +18,31 @@ int main(){
 
 
 void Fork-variable-test() {
+	pid_t childPID;	
+	int var = 10;
 
+	childPID = fork();
+
+	if (childPID >= 0){ //successful fork
+		if(childPID == 0){ //child process
+			printf("Variable value: %d\n", var);
+			var++;
+			printf("Independent variable value: %d\n", var);
+		}
+		else { //parent process
+			printf("Variable value: %d\n", var);
+			var = var+10;
+			printf("Independent variable value: %d\n", var);
+		}
+	}
+	else { //unsuccessful fork
+		printf("Fork failed\n");
+		return 1;
+	}
 }
 
 void File-descriptor-test() {
-	
+	pid_t childPID;
+
+	childPID = fork();
 }
