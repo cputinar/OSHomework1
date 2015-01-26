14d74037e365b789bd73806d018220d310f3aec3
aggiedev7 <aggiepax@gmail.com>
2015-01-25 16:24:57 -0800

    program 4 code added

--- Program4.c
+++ Program4.c
@@ -1,9 +1,39 @@
+#include <signal.h>
+#include <uninstd.h>
 #include <stdio.h>
 
-int AlrmTst() {
+int main(){
 
+	static struct sigaction act;
+	int count = 0;
+	pid_t pid, ppid;
+
+	void catchint(int);
+	act.sa_handler = catchint;
+	sigfillset(&(act.sa_mask));
+	sigaction(SIGALRM, &act, NULL);  
+
+	ppid = getpid();
+	pid = fork();
+	if(pid != 0)
+		pid = fork();
+
+	if(ppid != getpid()){
+		while(1){
+			count++;
+		}
+	}
+	else{
+		for(int i = 0; i < 5; i++){
+			sleep(1);
+			kill(pid1, SIGALRM);
+			kill(pid2, SIGALRM);
+
+		}		
+	}		
 }
 
-int main(){
-	
+void catchint(int sig){
+	printf("Count is %d, my pid is %d\n", count, getpid());
+	count = 0;
 }
